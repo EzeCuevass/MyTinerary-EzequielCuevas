192cc5f9f008b19f6fba3279bacb5e162d6d67e1
gh0s7face <eze.cuevass@gmail.com>
2022-06-08 00:16:01 -0300

    Navbar y Slider

--- mytinerary/src/App.js
+++ mytinerary/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
 import './styles/App.css';
+import Slider from './components/Slider';
 
 function App() {
   return (
     <div className="App">
-      <p>Hola mundo</p>
+      <Navbar />
+      <Slider />
+      {/* <p>Hola mundo</p> */}
     </div>
   );
 }
